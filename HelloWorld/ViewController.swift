3c3410151e093388c07be36f51f78486501f8439
Shay-Jahen Merritte <Shay@Merritte.com>
2016-10-17 18:03:45 -0400

    updated button design and added additional notifications

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -24,9 +24,24 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "welcome to my first app", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         present(alertController, animated: true, completion: nil)
-        
-        
-        
+    }
+    
+    @IBAction func monkeySee() {
+        let alertController = UIAlertController(title: "Monkey See", message: "See No Evil", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "booo", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func monkeyHear() {
+        let alertController = UIAlertController(title: "Monkey Hear", message: "Hear No Evil", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "ahhh", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func monkeySpeak() {
+        let alertController = UIAlertController(title: "Monkey Speak", message: "Speak No Evil", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "shh", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 
 
